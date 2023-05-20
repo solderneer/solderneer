a6a6ed047c29abf1bd4248ed87d2b9dded3e5df8
Sudharshan <sudhar393@gmail.com>
2023-05-20 08:23:55 +0100

    added more features into the graph

--- site/integrations/astrojs-indexmd.ts
+++ site/integrations/astrojs-indexmd.ts
@@ -3,48 +3,53 @@ import { fileURLToPath } from "node:url";
 import fs from "fs/promises";
 import cheerio from "cheerio";
 
-interface LinkNode {
+interface Link {
   source: string;
   target: string;
+}
+
+interface Node {
+  id: string;
   name: string;
 }
 
-interface LinkIndex {
-  forwardLinks: Record<string, LinkNode[]>;
-  backLinks: Record<string, LinkNode[]>;
-  allLinks: LinkNode[];
+interface Index {
+  forwardLinks: Record<string, Link[]>;
+  backLinks: Record<string, Link[]>;
+  nodes: Node[];
 }
 
-function findLinksInHTML(source: string, html: string): LinkNode[] {
+function findLinksInHTML(source: string, html: string): [Link[], Node] {
   // Load the HTML into Cheerio
   const $ = cheerio.load(html);
 
   // Find all anchor tags inside markdown blocks
-  const links: LinkNode[] = [];
+  const links: Link[] = [];
+
   $(".md-block a").each((_, element) => {
-    const link = $(element).attr("href");
+    let link = $(element).attr("href");
     const name = $(element).text();
     if (link) {
       // Filter out external links
-      const regex = /^\/[^/]+/;
-      if (source === link.substring(1) + "/") {
+      link = link.replace(/^\/+/, "") + "/";
+      const regex = /^notes\//;
+      if (source === link) {
         // The source and dest are the and and we can skip it
       } else {
         if (regex.test(link)) {
           links.push({
             source: source,
-            target: link.substring(1) + "/", // Have to do this weird thing so it's consistently formatted
-            name: name,
+            target: link, // Have to do this weird thing so it's consistently formatted
           });
         }
       }
     }
   });
-  return links;
+  return [links, { id: source, name: $("#title").text().trim() }];
 }
 
-function groupLinksBySource(list: LinkNode[]): Record<string, LinkNode[]> {
-  const groupedElements: Record<string, LinkNode[]> = {};
+function groupLinksBySource(list: Link[]): Record<string, Link[]> {
+  const groupedElements: Record<string, Link[]> = {};
 
   for (const item of list) {
     if (groupedElements[item.source]) {
@@ -57,8 +62,8 @@ function groupLinksBySource(list: LinkNode[]): Record<string, LinkNode[]> {
   return groupedElements;
 }
 
-function groupLinksByDestination(list: LinkNode[]): Record<string, LinkNode[]> {
-  const groupedElements: Record<string, LinkNode[]> = {};
+function groupLinksByDestination(list: Link[]): Record<string, Link[]> {
+  const groupedElements: Record<string, Link[]> = {};
 
   for (const item of list) {
     if (groupedElements[item.target]) {
@@ -77,24 +82,30 @@ const createPlugin = (): AstroIntegration => {
     hooks: {
       "astro:build:done": async ({ dir, routes, pages }) => {
         // For each of the pages, find the file and then find the links in the page
-        let linkNodes: LinkNode[] = [];
+        let linkList: Link[] = [];
+        let nodeMap: Record<string, Node> = {};
+        const regex = /^notes\//;
         for (const page of pages) {
+          if (!regex.test(page.pathname)) continue;
+
           const filePath = fileURLToPath(
             new URL(page.pathname + "index.html", dir)
           );
 
-          console.log(filePath);
-
           const data = await fs.readFile(filePath, { encoding: "utf-8" });
-          const links = findLinksInHTML(page.pathname, data);
-          linkNodes = [...linkNodes, ...links];
+          const [links, node] = findLinksInHTML(page.pathname, data);
+          linkList = [...linkList, ...links];
+          console.log(node);
+          nodeMap[page.pathname] = node;
         }
 
-        const index: LinkIndex = {
-          forwardLinks: groupLinksBySource(linkNodes),
-          backLinks: groupLinksByDestination(linkNodes),
-          allLinks: linkNodes,
+        const index: Index = {
+          forwardLinks: groupLinksBySource(linkList),
+          backLinks: groupLinksByDestination(linkList),
+          nodes: nodeMap,
         };
+
+        console.log(index);
 
         const indexJson = JSON.stringify(index);
         const writePath = fileURLToPath(new URL("mdindex.json", dir));
@@ -104,8 +115,6 @@ const createPlugin = (): AstroIntegration => {
         } catch (err) {
           console.error("Error writing JSON file:", err);
         }
-
-        console.log(index);
       },
     },
   };
